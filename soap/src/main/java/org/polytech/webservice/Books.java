d8add3f760ab47322dfc4405ba5be6e34f836568
MattPoly <matthieu.cayet@etu.univ-lyon1.fr>
2019-10-24 17:53:24 +0200

    client

---
+++ soap/src/main/java/org/polytech/webservice/Books.java
@@ -0,0 +1,69 @@
+
+package org.polytech.webservice;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Classe Java pour books complex type.
+ * 
+ * <p>Le fragment de schéma suivant indique le contenu attendu figurant dans cette classe.
+ * 
+ * <pre>
+ * &lt;complexType name="books">
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="elements" type="{http://spring.io/guides/gs-producing-web-service}book" maxOccurs="unbounded"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "books", propOrder = {
+    "elements"
+})
+public class Books {
+
+    @XmlElement(required = true)
+    protected List<Book> elements;
+
+    /**
+     * Gets the value of the elements property.
+     * 
+     * <p>
+     * This accessor method returns a reference to the live list,
+     * not a snapshot. Therefore any modification you make to the
+     * returned list will be present inside the JAXB object.
+     * This is why there is not a <CODE>set</CODE> method for the elements property.
+     * 
+     * <p>
+     * For example, to add a new item, do as follows:
+     * <pre>
+     *    getElements().add(newItem);
+     * </pre>
+     * 
+     * 
+     * <p>
+     * Objects of the following type(s) are allowed in the list
+     * {@link Book }
+     * 
+     * 
+     */
+    public List<Book> getElements() {
+        if (elements == null) {
+            elements = new ArrayList<Book>();
+        }
+        return this.elements;
+    }
+
+}
---
+++ soap/src/main/java/org/polytech/webservice/GetBooksResponse.java
@@ -0,0 +1,64 @@
+
+package org.polytech.webservice;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Classe Java pour anonymous complex type.
+ * 
+ * <p>Le fragment de schéma suivant indique le contenu attendu figurant dans cette classe.
+ * 
+ * <pre>
+ * &lt;complexType>
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="books" type="{http://spring.io/guides/gs-producing-web-service}books"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "", propOrder = {
+    "books"
+})
+@XmlRootElement(name = "getBooksResponse")
+public class GetBooksResponse {
+
+    @XmlElement(required = true)
+    protected Books books;
+
+    /**
+     * Obtient la valeur de la propriété books.
+     * 
+     * @return
+     *     possible object is
+     *     {@link Books }
+     *     
+     */
+    public Books getBooks() {
+        return books;
+    }
+
+    /**
+     * Définit la valeur de la propriété books.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link Books }
+     *     
+     */
+    public void setBooks(Books value) {
+        this.books = value;
+    }
+
+}
